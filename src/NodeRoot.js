d00d33412bb823d78716a196be9264d856c52631
Nyaksik <makapori@gmail.com>
2021-10-19 00:27:36 +0700

    well done 2.0

--- src/NodeRoot.js
+++ src/NodeRoot.js
@@ -4,7 +4,7 @@ function NodeRoot({ id, name, children, clickNode }) {
     return (
         <div>
             <div className='root' onClick={() => clickNode(id, name)}>{name}</div>
-            <div className='node'>
+            <div className='node' tabIndex='0'>
                 {(children || []).map((it) => <NodeRoot {...it} clickNode={clickNode} />)}
             </div>
         </div>
